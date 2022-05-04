bb0c8b4ba2f6c7f45c7372842e93d7174bd66380
Al1Santa <alain.santamaria57@gmail.com>
2022-05-04 10:10:18 +0200

    Crud

---
+++ src/Form/EventType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('link_video')
+            ->add('link_sound')
+            ->add('picture_1')
+            ->add('picture_2')
+            ->add('picture_3')
+            ->add('time')
+            ->add('age')
+            ->add('category')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
