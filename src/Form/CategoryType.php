8e3b22b531065d0c140ed5d67781aa5695be4b6a
Al1Santa <alain.santamaria57@gmail.com>
2022-05-06 15:21:48 +0200

    create branch category

--- src/Form/CategoryType.php
+++ src/Form/CategoryType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +13,11 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('events')
+            ->add('name', TextType::class, 
+            [
+                "label" => "Nom de catégorie :",
+            ])
+            // ->add('events')
         ;
     }
 
