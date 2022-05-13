d1ac1aa10418e070e403f651bab6ec65e7b8714b
Al1Santa <alain.santamaria57@gmail.com>
2022-05-13 09:31:38 +0200

    API create user

--- src/Controller/Api/UserController.php
+++ src/Controller/Api/UserController.php
@@ -63,6 +63,93 @@ class UserController extends AbstractController
     );
     }
 
+
+
+
+    /**
+     * Crée un utilisateur
+     * 
+    * @Route("/api/user/create", name="app_api_user_create", methods={"POST"})
+     * 
+     * @OA\RequestBody(
+     *     @Model(type=UserType::class)
+     * )
+     * 
+     * @OA\Response(
+     *     response=201,
+     *     description="new created user",
+     *     @OA\JsonContent(
+     *          ref=@Model(type=User::class, groups={"show_users"})
+     *      )
+     * )
+     * 
+     * @param Request $request infos venant de mon front/utilisateur
+     * @param EntityManagerInterface $em
+     * @param SerializerInterface $serializerInterface Permet de transformer du JSON en Objet
+     * @return JsonResponse
+     */
+    public function create(
+        Request $request,
+        EntityManagerInterface $em,
+        SerializerInterface $serializerInterface,
+        ValidatorInterface $validator
+        ): JsonResponse
+    {
+        // TODO : Request parce que je vais recevoir des données
+        $jsonContent = $request->getContent();
+        // dump($jsonContent);
+        // je transforme ces données en Entité
+        //! la deserialisation ne respecte aucune règle (Assert sur notre entité)
+        //? notre serializer peut faire des erreurs son notre front/utilisateur nous envoi du JSON mal formé
+        try { // on espère que le serializerInterface arrive à relire le JSON
+            $user = $serializerInterface->deserialize($jsonContent, User::class, 'json');
+        } catch(Exception $e){ 
+            //  si le serializerInterface n'arrive pas à relire le JSON on saute directement dans la partie Catch
+            
+            // erreur 422 : on ne peut pas traiter les infos qu'ils nous a donné
+            return $this->json(
+                "JSON mal formé",
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+            
+        // TODO : utiliser les Assert de notre entité pour valider la deserialisation
+        // le validator nous renvoit la liste de toutes les erreurs
+        $errorList = $validator->validate($user);
+
+        //je teste si il y a des erreurs 
+        if (count($errorList) > 0){
+            // j'ai des erreurs, l'utilisateur/front n'a pas respecter les Assert
+            //? version bourrine : je transforme le tableau en chaine
+            $errors = (string) $errorList;
+
+            // erreur 422 : on ne peut pas traiter les infos qu'ils nous a donné
+            return $this->json(
+                $errors,
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+
+        // TODO : entityManagerInterface pour l'enregistrement
+        $em->persist($user);
+        $em->flush();
+        // TODO : return json avec le bon code 201 (created)
+        return $this->json(
+            $user,
+            // je précise que tout est OK de mon coté en précisant que la cration c'est bien passé
+            // 201
+            Response::HTTP_CREATED,
+            [],
+            [
+                "groups" => 
+                [
+                    "show_users"
+                ]
+            ]
+        );
+    }
+
+
     /**
      * Modifier les données utilisateur
      * 
